4342b828657b6341997d3b47e4beac3e576e02ad
abrarasif11 <faasif929@gmail.com>
2022-12-04 00:10:09 +0600

    router-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home';
+import Review from './Pages/Reviews/Review';
+import MyReview from './Pages/Reviews/MyReview';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element : <Home></Home>
+        },
+        {
+          path: '/reviews',
+          element : <Review></Review>
+        },
+        {
+          path: '/myReviews',
+          element : <MyReview></MyReview>
+        },
+        {
+          path: '/aboutUs',
+          element : <AboutUs></AboutUs>
+        },
+      ]
+    },
+    {
+      path: '*',
+      element:<ErrorPage></ErrorPage>
+    }
+  ])
   return (
     <div className="App">
-     
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
