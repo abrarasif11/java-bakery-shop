09b5f4e5468b0697906dbb09ab75f28e28216fb8
abrarasif11 <faasif929@gmail.com>
2022-12-23 15:25:59 +0600

    order-section

--- src/Pages/Dashboard/DashboardLayout.js
+++ src/Pages/Dashboard/DashboardLayout.js
@@ -20,7 +20,7 @@ const DashboardLayout = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
 
-                        <li><Link to="/dashboard/allUsers" className='text-2xl'>All Sellers</Link></li>
+                        <li><Link to="/dashboard/allUsers" className='text-2xl'>All Users</Link></li>
 
                         <li><Link to='/dashboard' className='text-2xl'>My Orders</Link></li>
 
